08bd399984f10a5943563a2987d5de7b1113876c
marcosajc <marcosajc@gmail.com>
2017-05-11 17:34:59 -0400

    Latest changes of script to send NDT towards collector

---
+++ measure_ndt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+######### 	measure_ndt.sh	###########
+#
+#	Executes the NDT test provided by measurement kit.
+#
+#	Recieves the probeid as argument.
+
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+PROBEID=$1
+
+cd $SCRIPTPATH
+
+# Executes the NDT test and sends the report.
+./measurement_kit -a probeid=$PROBEID ndt
+#-c httpo://vowqsydjizhxdr67.onion ndt
+
+# Removes all njson created.
+#rm *.njson
